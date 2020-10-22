b695b2990e2de8c428e27f82278729bc782b82c3
Pablo Roda <pablojoseroda63@gmail.com>
2020-10-22 11:04:32 -0300

    Creacion de pantallas basicas

---
+++ Iceberg/IniciarSesion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Iceberg
+{
+    public partial class frmIniciarSesion : Form
+    {
+        public frmIniciarSesion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.ShowInTaskbar = false;
+            new Panel().Show();
+        }
+    }
+}
